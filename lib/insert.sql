995c84e15366c294db4a9b0c410156af78e799a7
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-23 02:06:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "White", "Laxed", 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 24, "M", "lavender", "angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "lavender", "angry", 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "M", "Brown", "Happy", 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2000, "F", "Black", "Anxious", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "M", "Pink", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "M", "Pink", "Angry", 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Blue", "Chill", 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "F", "Red", "Chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Red", "Chill", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9000, "F", "Grey", "Happy", 0);
