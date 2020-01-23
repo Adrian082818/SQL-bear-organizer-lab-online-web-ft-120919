62449062b04987496f3e2b98b965440fefdee8f8
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-23 01:25:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,14 +2,14 @@ INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament 
 
 INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Rowdy", 24, "Male", "lavender", "angry", "0");
 
-INSERT INTO (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Tabitha", 6, "Male", "Brown", "Happy", "0");
+INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Tabitha", 6, "Male", "Brown", "Happy", "0");
 
-INSERT INTO (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Sergeant Brown", 2000, "Female", "Black", "Anxious", "1
+INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Sergeant Brown", 2000, "Female", "Black", "Anxious", "1
 
-INSERT INTO (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Melissa", 1, "Male", "Pink", "Angry", "0");
+INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Melissa", 1, "Male", "Pink", "Angry", "0");
 
-INSERT INTO (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Grinch", 100, "Male", "Blue", "Chill", "0");
+INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Grinch", 100, "Male", "Blue", "Chill", "0");
 
-INSERT INTO (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Wendy", 5, "Female", "Red", "Chill", "1");
+INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES ("Wendy", 5, "Female", "Red", "Chill", "1");
 
-INSERT INTO (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES (NULL, 9000, "Female", "Grey", "Happy", "0");
+INSERT INTO bears (name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN) VALUES (NULL, 9000, "Female", "Grey", "Happy", "0");
